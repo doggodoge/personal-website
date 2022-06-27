84fc39cf0403c4f4befd9eb1019bad5a9859be41
Gary Moore <madmangaz@gmail.com>
2022-06-27 17:30:49 +0100

    fix: remove header component and move into App

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,14 @@ import style from './App.module.css'
 import { BlogPost, Social } from './components'
 import { posts } from './posts'
 
-const Header: Component = () => {
-  return (
-    <header class={style.header}>
-      <h1>Gary's Website!</h1>
-      <Social github="https://github.com/doggodoge" email="gary@garymoore.ie" />
-    </header>
-  )
-}
-
 const App: Component = () => {
   return (
     <div class={style.app}>
-      <Header />
+      <header class={style.header}>
+        <h1>Gary's Website!</h1>
+        <Social github="https://github.com/doggodoge" email="gary@garymoore.ie" />
+      </header>
+
       <div class={style.contentContainer}>
         <For each={posts}>
           {blog => <BlogPost content={blog} />}
