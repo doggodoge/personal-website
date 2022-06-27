842b66857edb2b7ca95e4430614fe03c6a01d08f
Gary Moore <madmangaz@gmail.com>
2022-06-27 02:53:44 +0100

    fix: move posts into own file

---
+++ src/posts.ts
@@ -0,0 +1,81 @@
+const blogOne = `
+# Autem moderatior sacrorum idem non
+
+## Tuaque commenta caecae altera dat hominum reponit
+
+Lorem *markdownum notum* pharetram, praestantissima vastum petis iter lacus
+docta, non audit vulnus, parabat fecunda inscius. Secum haec mixta, quondam
+cuius consolenturne corpus perarantem relinque vinci, illa. Pro *rates pallentia
+ipsum* agnae quoque, Autolycus laboriferi Aeacon talia curvantem saepe tergaque
+sede infelix animumque famaque flumina. Nec hostem certe modo ingratos in quae
+Medusae media lutulenta delapsam tumulum iam *illos ante* oculos et nisi. Est
+satis ictaque ante eunti instantem niveisque liber gerit, de nives tuis quidem
+utraque reddere instruxit fraude letum.
+
+## Furentem ora fretum scitis novo hordea
+
+Omnique cerae fieri tibi Helicen. Audiri patriae laudatis me Indus duabus,
+[tibi](http://athamanteosque.com/) sic molimine parte dedit narrat per, illo.
+Cortice quae temerare scopulum remansit, si artus iacet bis frequens fatentis.
+Sed tridente navigat? Mota est tuarum volucres caro purpureum, erat tanta suem
+honore ardua omnesque sortes Troada, ne.
+
+## Mora est Titania humilem ventis et litora
+
+Saxo rettulit, qui telo augurium adversi anum Aiaci inmota. **Repetiti cui
+pallor** fugis, coniugis deorum vertitur, sub. Quid iniectos suscipimus tenere
+urit datis generis tamen. Themin lecto officio: Morphea verterit, late caecis,
+fera.
+
+1. Corpora polo vivere tibi invidiosa est igne
+2. Parva membra et risit
+3. Mollem nullo illic corpore conata propositum haesit
+4. Profuit mihi tu manum armis
+5. Spondere vestem revulsus Inachidos sua dapibus ad
+6. Et Othryn caelesti moram
+
+## Quaeque posse humilis illic mihi dumque medio
+
+Cum secantes virorum, proles Scythicas tutaeque sensum, ictus? Transtulit
+edidicitque temeraria, *non* silva artes auxilium labefactum eicit *quod lucem
+iniceret* celebrabant extremum agros insidiaeque vidit inmeritam. Dederit
+Phineu, tauros, deus et *haustos* quisque, ecce tollens deae: dicere. Sine hic
+gladios, ait omnia!
+
+## Ubera moenibus Veneris sis sponte titubantem tu
+
+Posset *solis*; numenque est [inque haec
+mihi](http://quam-quaerenti.com/caelum.html) veluti; quamvis ne. Oculis firmo
+curribus formatus in Helicen gravis. Genus *nidis* alga arcet maximus monte
+Iunonem tuo. Pendere pendet verba modo, Babylonia Phasidos et magni ut.
+
+    if (quicktime.bookmark(cdStick.spoolPassword(southbridgeDayMultiplatform(
+            resolutionIpSwitch, 23, -4), basic_d_read, isdnDesign))) {
+        media_rdf_smartphone.wikiUser.flashDdlMebibyte(scsiGbps.responsive_arp(
+                pseudocodeToken), firewall);
+        modelOsd.dimmCardPlain = 5 + ibm_gnutella;
+    }
+    if (market_access_ppm) {
+        computerUserSpooling += directoryAdapterDel;
+    }
+    ccd = stackVirtual;
+    if (log) {
+        storage_oop += bare(64);
+    }
+    cdFontFragmentation += device_adf + basic_eps_p / finder(mask_vpn, 1, scan);
+
+Gravis tenet vulnera **ita** in virentem tui **posti Phoebo**. Orbem sola omnia
+quoniam Lucifer, Iove cum secernit tibi. Venit senex: quaeque est mundi suis,
+cum sacrorum, armatus.
+`.trim()
+
+const blogTwo = `
+# This is a second test post, or whatever
+
+> How do we do quotes?
+
+- Some of this two, how?
+- And some of this?
+`.trim()
+
+export const posts = [blogOne, blogTwo]
