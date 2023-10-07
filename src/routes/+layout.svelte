61b3da8c184a9785d258249aceac91fdbd9ca4b8
Gary Moore <gary@garymoore.ie>
2023-10-07 15:44:17 +0100

    fix: make the nav a little better and remove emoji

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,7 @@
     display: flex;
     flex-direction: row;
     width: 100%;
-    height: 100%;
+    height: 100vh;
   }
 
   .content {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,14 @@
-<script>
-  import FunkyEmoji from '$lib/FunkyEmoji.svelte';
-</script>
-
 <main>
-  <h1>Gary Moore's Personal Website</h1>
-  <div>
-    <FunkyEmoji emoji="🏗️" size="5rem" />
-    <FunkyEmoji emoji="👷" size="5rem" />
-    <FunkyEmoji emoji="🚧" size="5rem" />
-  </div>
-  <p><em>Under Construction</em>.</p>
+	<h1>Gary Moore's Personal Website</h1>
+	<p>
+		You can still check out some headings on the left-hand side, but there's no real content to this
+		site yet. Maybe check back some other time.
+	</p>
 </main>
 
 <style>
-  main {
-    justify-content: center;
-    text-align: center;
-  }
+	main {
+		justify-content: center;
+		text-align: center;
+	}
 </style>
